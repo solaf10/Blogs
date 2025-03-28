c5b1312953f22bb2be288392b0df815296f39449
solaf10 <Soso103m@gmail.com>
2025-03-28 22:19:49 +0300

    fixing images path

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -4,37 +4,37 @@ import "./Blogs.css";
 const Blogs = () => {
   const cards = [
     {
-      image: "/public/assets/imgs/blog1.png",
+      image: "/assets/imgs/blog1.png",
       author: "Alec Whitten • 1 Jan 2023",
       title: "Bill Walsh leadership lessons",
       desc: "Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?",
     },
     {
-      image: "/public/assets/imgs/blog2.png",
+      image: "/assets/imgs/blog2.png",
       author: "Demi WIlkinson • 1 Jan 2023",
       title: "PM mental models",
       desc: "Mental models are simple expressions of complex processes or relationships.",
     },
     {
-      image: "/public/assets/imgs/blog3.png",
+      image: "/assets/imgs/blog3.png",
       author: "Candice Wu • 1 Jan 2023",
       title: "What is Wireframing?",
       desc: "Introduction to Wireframing and its Principles. Learn from the best in the industry.",
     },
     {
-      image: "/public/assets/imgs/blog4.png",
+      image: "/assets/imgs/blog4.png",
       author: "Natali Craig • 1 Jan 2023",
       title: "How collaboration makes us better designers",
       desc: "Collaboration can make our teams stronger, and our individual designs better.",
     },
     {
-      image: "/public/assets/imgs/blog5.png",
+      image: "/assets/imgs/blog5.png",
       author: "Drew Cano • 1 Jan 2023",
       title: "Our top 10 Javascript frameworks to use",
       desc: "JavaScript frameworks make development easy with extensive features and functionalities.",
     },
     {
-      image: "/public/assets/imgs/blog6.png",
+      image: "/assets/imgs/blog6.png",
       author: "Orlando Diggs • 1 Jan 2023",
       title: "Podcast: Creating a better CX Community",
       desc: "Starting a community doesn’t need to be complicated, but how do you get started?",
